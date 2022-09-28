7807a8df0bc1421fb837660103dd58c444c7c4fc
Cesar H. de Oliveira <111370768+getsugate@users.noreply.github.com>
2022-09-28 16:42:23 -0300

    Add files via upload

---
+++ bytebank-herdado-abstract/src/Funcionario.java
@@ -0,0 +1,32 @@
+//classe abstract não se pode instanciar metodos da da mesma
+public abstract class Funcionario {
+	
+	private String nome;
+	private String cpf;
+	private double salario;
+	//metodo abstract o metodo não tem corpo
+	public  abstract double getBonificacao();
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+	
+
+}
