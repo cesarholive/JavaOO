554ca535e043aada07a3c6d55f91da5ad174e572
Cesar H Oliveira <cesar_ho1993@hotmail.com>
2022-09-30 12:18:26 -0300

    composição

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,27 +1,27 @@
 // extends herda atributos da classe funcionário, assina o contrato autenticavel
-public class Gerente extends Funcionario implements Autenticavel{
+public class Gerente extends Funcionario implements Autenticavel {
 
-	private int senha;
-	
-	public double getBonificacao() { // metodo para somar 10% no salario em caso de bonificação.
-		System.out.println("bonificacao gerente");
-		return super.getBonificacao() + super.getSalario(); // super é utilizado no lugar do this pois o atributo
-															// salario é da classe mãe
+	private AutenticacaoUtil autenticador;
+
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
 	}
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
-		
+		this.autenticador.setSenha(senha);
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		}else {
-		return false;
-		}
+		return this.autenticador.autentica(senha);
+
+	}
+
+	public double getBonificacao() { // metodo para somar 10% no salario em caso de bonificação.
+		System.out.println("bonificacao gerente");
+		return super.getBonificacao() + super.getSalario(); // super é utilizado no lugar do this pois o atributo
+															// salario é da classe mãe
 	}
 
 }
