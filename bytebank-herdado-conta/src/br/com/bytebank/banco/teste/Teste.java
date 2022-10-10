6f3b090b183e040c509411efcaa641a26d32dab4
Cesar H Oliveira <cesar_ho1993@hotmail.com>
2022-10-10 16:50:39 -0300

    teste java

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -1,24 +1,7 @@
 package br.com.bytebank.banco.teste;
 
-import br.com.bytebank.banco.modelo.Cliente;
-import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.ContaPoupanca;
-
 public class Teste {
-
 	public static void main(String[] args) {
-		System.out.println("x");
-		System.out.println(3);
-		System.out.println(false);
-		
-		
-		ContaCorrente cc = new ContaCorrente(22, 33);
-		Object cp = new ContaPoupanca(1, 2);
-		Object cli = new Cliente();
-		
-		System.out.println(cc.toString());
-		System.out.println(cp);
 		
 	}
-
 }
